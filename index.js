15a7cf5b142646ab2c660aa8cf20ba6f2748330c
farida-shawky <faridashawky@gmail.com>
2026-09-14 21:46:47 +0300

    main

--- index.js
+++ index.js
@@ -1,17 +1,17 @@
-var result = document.getElementById("result");
-result.style.color = "red";
+var allbuttons = document.querySelectorAll("button");
+var _div = document.querySelector("div");
 
-function increment() {
-  +result.innerHTML++;
-  result.style.color = "blue";
-}
-
-function decrement() {
-  if (result.innerHTML <= 1) {
-    result.innerHTML = "0";
-    result.style.color = "red";
-  } else {
-    +result.innerHTML--;
-    result.style.color = "blue";
-  }
-}
+allbuttons.forEach((btns) => {
+  btns.addEventListener("click", (e) => {
+    if (e.target.classList.contains("btn-danger")) {
+      e.target.parentNode.classList.remove("bg-primary", "bg-dark");
+      e.target.parentNode.classList.add("bg-danger");
+    } else if (e.target.classList.contains("btn-primary")) {
+      e.target.parentNode.classList.remove("bg-danger", "bg-dark");
+      e.target.parentNode.classList.add("bg-primary");
+    } else if (e.target.classList.contains("btn-dark")) {
+      e.target.parentNode.classList.remove("bg-danger", "bg-primary");
+      e.target.parentNode.classList.add("bg-dark");
+    }
+  });
+});
